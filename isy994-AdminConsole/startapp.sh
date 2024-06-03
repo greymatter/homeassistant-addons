2a3e122bb664ce4acfdee9166376f6fec4a593f8
greymatter <s4biturbo@gmail.com>
2024-06-03 11:16:00 -0400

    dockerfiles

---
+++ isy994-AdminConsole/startapp.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+echo "Starting"
+
+if [ -z "${ISY994_HOST}" ]; then
+    echo "${RED}Please set a proper isy994 host with ISY994_HOST${NC}"
+    sleep 2
+    exit 1
+fi
+
+echo "Environment ok"
+
+cd /app
+
+if [ ! -f app/admin.jnlp ]; then
+    echo "Downloading admin.jnlp"
+
+    wget -O admin.jnlp http://${ISY994_HOST}/admin.jnlp
+
+    if [ ! $? -eq 0 ]; then
+        echo "${RED}Failed to download admin.jnlp, please check your settings${NC}"
+        sleep 2
+        exit 2
+    fi
+fi
+
+echo "${GREEN}Initialization complete, starting virtual console${NC}"
+itweb-settings -set deployment.manifest.attributes.check NONE
+
+exec javaws /app/admin.jnlp
